adea60eb74dc0c3a15c58309e066a4c54ad4fdc4
zhoufuqi <zhou_fu_qi@foxmail.com>
2025-04-30 17:07:36 +0800

    配置 starship 代理显示

--- shell/.scripts/alias.bash
+++ shell/.scripts/alias.bash
@@ -53,4 +53,3 @@ function y() {
 alias setproxy='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
 alias resetproxy="export https_proxy='' http_proxy='' all_proxy=''"
 setproxy
-# resetproxy
